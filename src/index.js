07441f2314973bf4d539244045a60a834ee23b1e
Sudhanshu Gupta <112060005+SudhanshuGupta409@users.noreply.github.com>
2023-08-18 15:12:38 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color="blue"
+        maxRating={10}
+        size={24}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie is rated {movieRating} stars.</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={1}
+    />
+    <StarRating color="red" size={30} maxRating={10} />
+
+    <Test />
+  </React.StrictMode>
+);
